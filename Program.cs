58b3e3e8f9405f0ffa5ba8c5f5b77a7cd7fbdf46
KelwinGerson <83376362+KelwinGerson@users.noreply.github.com>
2022-06-20 19:04:30 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace first_program
+{
+    class Program {
+        // write in console
+        static void Main(string [] args ) {
+            Console.WriteLine("Hello, World!"); 
+        }
+    }
+}
+
+
